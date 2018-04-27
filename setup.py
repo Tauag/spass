0241a250347760515035bdddfecd8122d85bca78
Gavin Li <itsgavin.li@gmail.com>
2018-04-26 23:13:38 -0400

    setup.py modifications

--- setup.py
+++ setup.py
@@ -4,9 +4,20 @@ setup(name='spass',
       version='0.1',
       description='Simple and Secure passphrases',
       keywords='cryptography password passphrase secure',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'License :: OSI Approved :: MIT License',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python :: 3'],
+      project_urls={
+          'Source': 'https://github.com/Tauag/spass',
+          'Tracker': 'https://github.com/Tauag/spass/issues'
+      },
+      python_requires='>=3',
+
       url='https://github.com/Tauag/spass',
       author='Tauag',
       author_email='dev.gavin.li@gmail.com',
+
       license='MIT',
       packages=find_packages(),
       install_requires=[],
